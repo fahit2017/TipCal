3b5fda579582e5d72cef58d86aaf72552fdc4137
Fahit Ahmed <fahit.ahmed@baruchmail.cuny.edu>
2018-09-08 01:30:55 -0400

    TipCalculator Assignment

--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    @IBAction func CalculateTip(_ sender: Any) {
+       
+        let tipPercentages = [0.20, 0.18, 0.15]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+
+        tipLabel.text = String(format: "$%.2f", tip )
+        totalLabel.text = String(format: "$%.2f", total )
+    }
+
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
